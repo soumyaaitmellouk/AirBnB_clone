3e1a4bbb07866b682b08e10e78b78e889b35f552
soumyaaitmellouk <soumyaaitmellouk222@gmail.com>
2024-02-10 13:36:26 -0800

    third try

--- console.py
+++ console.py
@@ -2,26 +2,21 @@
 """Defines the HBnB console."""
 import cmd
 
+
 class HBNBCommand(cmd.Cmd):
     """
     the HolbertonBnB command interpreter.
     """
     prompt = "(hbnb) "
 
-
     def do_quit(self, arg):
         """ exit the program."""
         return True
+
     def do_EOF(self, line):
         """Exit the console """
         print()
         return True
-
-
-
-
-
-
 
 
 if __name__ == '__main__':
