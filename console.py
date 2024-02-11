36656b066c985f648cf49a7675e4d6dcbfb4c17e
soumyaaitmellouk <soumyaaitmellouk222@gmail.com>
2024-02-11 02:12:33 -0800

    7try

--- console.py
+++ console.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Defines the HBnB console."""
 import cmd
+import re
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -9,14 +11,18 @@ class HBNBCommand(cmd.Cmd):
     """
     prompt = "(hbnb) "
 
-    def do_quit(self, arg):
+    def do_quit(self, line):
         """ exit the program."""
         return True
 
     def do_EOF(self, line):
         """Exit the console """
-        print()
+        print("")
         return True
+
+    def emptyline(self):
+        """Do nothing upon receiving an empty line."""
+        pass
 
 
 if __name__ == '__main__':
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,6 +5,9 @@ from models.base_model import BaseModel
 
 
 class FileStorage:
+    """
+    a storage engine 
+    """
     __file_path = "file.json"
     __objects = {}
 
@@ -14,24 +17,25 @@ class FileStorage:
 
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
-        self.__objects[obj.__class__.__name__, obj.id] = obj
-    
+        FileStorage.__objects["{}.{}".format(obj.__class__.__name__, obj.id)] = obj
+   
     def save(self):
         """Serialize __objects to the JSON file __file_path."""
         obj_dict = {}
         for key, obj in FileStorage.__objects.items():
-            obj_dict[f"{key[0]}-{key[1]}"] = obj.to_dict() 
+            obj_dict[key] = obj.to_dict()
+
         with open(FileStorage.__file_path, "w") as f:
             json.dump(obj_dict, f, default=str)
-    
+
     def reload(self):
         """Deserialize the JSON file __file_path to __objects, if it exists."""
         try:
             with open(FileStorage.__file_path) as f:
-                obj_dict = json.load(f)
-                for key, val in obj_dict.items():
-                    cls_name, obj_id = key.split(',')
-                    obj = eval(cls_name)(**val)
-                    self.__objects[key] = obj
+                objdict = json.load(f)
+                for o in objdict.values():
+                    cls_name = o["__class__"]
+                    del o["__class__"]
+                    self.new(eval(cls_name)(**o))
         except FileNotFoundError:
             return
